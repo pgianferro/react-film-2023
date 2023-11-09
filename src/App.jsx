fc1319a4405080149de2407c1f9f7ebebb1820fa
BurghiLucas8 <64329694+BurghiLucas8@users.noreply.github.com>
2023-11-09 20:30:33 -0300

    DESIGN + UAT

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Login from '../components/log_in/Login'
 
 const App = () => {
-  return (
-    <div>
+return (
+<div>
       <Login></Login>
-    </div>
-  )
+</div>
+)
 }
 
 export default App
