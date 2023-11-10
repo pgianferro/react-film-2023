752ed009f74425ae4b24323dbd12ecc8ecf2868d
BurghiLucas8 <64329694+BurghiLucas8@users.noreply.github.com>
2023-11-09 22:02:31 -0300

    reparando

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import NetflixImage from '../components/netfliximage/NetflixImage.jsx'
 import NetflixCards from '../components/netflixcards/NetflixCards.jsx' 
 import NetflixHeader from '../components/netflixheader/NetflixHeader.jsx'
 import { Divider, Spacer } from '@nextui-org/react'
-import Login from '../components/log_in/Login'
 
 const App = () => {
   return (
     <div>
-      <Login></Login>
       <NetflixNavbar></NetflixNavbar>
       <NetflixImage></NetflixImage>
       <Spacer y={10} ></Spacer>
