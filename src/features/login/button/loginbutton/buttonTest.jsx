1559d067d257c7d0b3d3e3f84d173af54c240886
BurghiLucas8 <64329694+BurghiLucas8@users.noreply.github.com>
2023-11-24 19:02:16 -0300

    loginview

--- src/features/login/button/loginbutton/buttonTest.jsx
+++ src/features/login/button/loginbutton/buttonTest.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ButtonTest() {
   return (
-    <div className="bg-red-700 inline-block rounded-md mt-8">
+    <div className="bg-red-700 inline-block rounded-md mt-8 shadow-md bg-opacity-90">
       <button type="submit" className="text-white px-4 py-2">
         Iniciar sesión
       </button>
