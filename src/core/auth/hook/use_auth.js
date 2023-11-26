b54dd926a1fbbe5d8f5b106ce4c18a8631593683
BurghiLucas8 <64329694+BurghiLucas8@users.noreply.github.com>
2023-11-26 08:11:02 -0300

    loginview

--- src/core/auth/hook/use_auth.js
+++ src/core/auth/hook/use_auth.js
@@ -3,12 +3,11 @@ import { AuthContext } from "../context/auth_context";
 
 //Acá se unen las 3 piezas: Conext, el Provider del Context y el hook que toma y retorna los valores del Provider.
 export const useAuth = () => {
-  const { isLoggedIn, login, logout, suscribe } = useContext(AuthContext);
+  const { isLoggedIn, login, logout, } = useContext(AuthContext);
 
   return {
     isLoggedIn,
     login,
     logout,
-    suscribe,
   };
 };
--- src/features/suscription/suscbutton/suscbutton.jsx
+++ src/features/suscription/suscbutton/suscbutton.jsx
@@ -2,17 +2,16 @@
 import Suschome from '../suschome/suschome';
 import React, { useState } from 'react';
 import { useAuth } from '../../../core/auth/hook/use_auth';
-import { Navigate } from 'react-router-dom'
 
 export default function App() {
     const { suscribe } = useAuth();
-   
+
     return (
         <div>
             ¿Primera vez en Netflix?
             <button
                 className='ml-1 text-default-800 relative overflow-hidden group'
-                onClick={ <Navigate to={"../suschome/suschome.jsx"}/> }
+                
             >
                 Suscríbete ahora!
                 <span className="absolute left-0 right-0 bottom-0 h-1 bg-white transition-transform transform scale-x-0 group-hover:scale-x-100"></span>
